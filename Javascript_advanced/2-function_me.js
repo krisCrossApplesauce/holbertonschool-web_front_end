54ddfb297fab743fbd203849c5b8d39e73a0a8a8
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-11-14 15:03:11 -0600

    task 2 :)

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+function welcomeMessage(fullName) {
+	return function () {alert(`Welcome ${fullName}`)}
+}
+
+const guillaume = welcomeMessage("Guillaume");
+const alex = welcomeMessage("Alex");
+const fred = welcomeMessage("Fred");
