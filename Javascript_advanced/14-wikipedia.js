c12b12156982c587c59f8c1d912728c5a6dc885d
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-11-15 17:25:02 -0600

    task 14 ,':| ...I don't understand this in the slightest

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,20 @@
+function createElement(data) {
+	let paragraph = document.createElement("p");
+	paragraph.textContent = data;
+	document.body.appendChild(paragraph);
+}
+
+function queryWikipedia(callback) {
+	let request = new XMLHttpRequest();
+
+	request.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*", true);
+
+	request.onload = function() {
+		let data = JSON.parse(request.responseText);
+		callback(data.query.extract);
+	};
+
+	request.send();
+}
+
+queryWikipedia(createElement);
