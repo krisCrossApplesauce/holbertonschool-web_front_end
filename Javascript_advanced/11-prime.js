68f9516f3be03bf62fc4f909483f623f23b4a060
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-11-15 16:27:23 -0600

    task 11?...
    I don't really understand what it means by "find a way to actually do most of the calculation at the end of the execution stack" bc it was already doing it at the end of the execution stack?? it's pretty much just as much at the end of the execution stack as it was before?? and the example given shows that it takes significantly less time (going from 40 millisecs to ~0.04), but mine:
    a- wasn't that slow in the first place
    b- didn't change the speed barely at all
    so I don't know what that's about ,'P

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,31 @@
+function countPrimeNumbers() {
+	let i, ii, x, n = 0;
+
+	for (i = 2; i <= 100; i++) {
+		x = 0;
+
+		for (ii = 2; ii < i && x == 0; ii++) {
+			if (i % ii == 0) {
+				x = 1;
+			}
+		}
+
+		if (x == 0) {
+			n++;
+		}
+	}
+
+	return (n);
+}
+
+const runTime = () => {
+	const t0 = performance.now();
+	for (i = 0; i < 100; i++) {
+		countPrimeNumbers();
+	}
+	const t1 = performance.now();
+
+	console.log(`Exectution time of calculating prime numbers 100 times was ${t1 - t0} milliseconds.`);
+}
+
+setTimeout(runTime, 0);
